e6921c1be4dbba8e5d56de0f793414f9dde65226
Evgeniya Bautina <janebarinova@gmail.com>
2015-10-11 02:01:59 -0700

    Add javadoc and fix coding style

--- Queue.java
+++ Queue.java
@@ -1,29 +1,56 @@
-import edu.princeton.cs.algs4.*;
-import java.lang.*;
+import edu.princeton.cs.algs4.StdOut;
 
-    
+/**
+ * A class representing a queue data structure implemented
+ * using an array.
+ * For example:
+ * <pre>
+ *    Queue q = new Queue(10);
+ *    StdOut.println(q);
+ * </pre>
+ *
+ * @author  Evgeniya Bautina
+ * @version 1.0 11 Oct 2015
+ */   
 public class Queue {
-    
     private int capacity;   
     private int [] q;    
     private int elemNum; //number of elements in the queue   
     private int head; // pointer to the first element in the queue
     private int tail; //pointer to the last element in the queue
     
+   /**
+    * Creates an empty queue.   
+    *
+    * @param  size  array size
+    */
     public Queue(int size) {
         capacity = size;
         q = new int[capacity];
     }
     
+   /**
+    * Checks a queue for emptiness. 
+    *
+    * @return      true if queue is empty
+    * @see         Queue
+    */
     public boolean isEmpty() { 
        return elemNum == 0; 
     }
     
+   /**
+    * Adds an element to the queue. If an array is full,
+    * calls resize funcion to double the array size.
+    *
+    * @param  elem  element to be added to the queue
+    * @see         Queue
+    */   
     public void enqueue(int elem) { //add an element to the queue
         if (elemNum == capacity) {
            resize();
         }
-        if (tail == capacity ) {
+        if (tail == capacity) {
             tail = 0;
             q[tail++] = elem;
             elemNum++;
@@ -35,13 +62,24 @@ public class Queue {
         }  
     }
     
-    public int dequeue() { //delete an element from the queue
+   /**
+    * Deletes an element from the queue. 
+    *
+    * @return      element deleted from the queue
+    * @see         Queue
+    */
+    public int dequeue() { 
         if (isEmpty())
             return 0;
         elemNum--;
         return q[head++];
     }
     
+   /**
+    * Doubles an array size then the array is full.  
+    *
+    * @see         Queue
+    */
     private void resize() { // double array size
         int oldCapacity = capacity;
         capacity *= 2;
@@ -70,10 +108,19 @@ public class Queue {
         tail = elemNum;
     }
     
+   /**
+    * Returns a number of elements in the queue. 
+    *
+    * @return      number of elements in the queue
+    * @see         Queue
+    */
     public int size() { // size of the queue
         return elemNum;
     }
     
+   /**
+    * Intended only for debugging.
+    */
     public String toString() {
         String st = "";
         for (int i = 0; i < capacity; i++) {
