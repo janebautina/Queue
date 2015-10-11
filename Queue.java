c27aff3a29d562d65379f2686f50def11938345b
Evgeniya Bautina <janebarinova@gmail.com>
2015-10-11 01:09:24 -0700

    Fix bugs in resize function

--- Queue.java
+++ Queue.java
@@ -1,78 +1,84 @@
-public class Queue{
+import edu.princeton.cs.algs4.*;
+import java.lang.*;
+
     
-    private int capacity;
+public class Queue {
     
-    private int [] a;
+    private int capacity;   
+    private int [] q;    
+    private int elemNum; //number of elements in the queue   
+    private int head; // pointer to the first element in the queue
+    private int tail; //pointer to the last element in the queue
     
-    private int elem_num;
-    
-    private int head;
-    
-    private int tail;
-    
-    public Queue(int size){
+    public Queue(int size) {
         capacity = size;
-        head = 0;
-        tail = 0;
-        a = new int[capacity];
+        q = new int[capacity];
     }
     
-    public boolean isEmpty() {
-       return elem_num == 0; 
+    public boolean isEmpty() { 
+       return elemNum == 0; 
     }
     
-    public void enqueue(int elem) {
-        if(elem_num == capacity){
-           capacity = capacity * 2;
-           resize(capacity);
+    public void enqueue(int elem) { //add an element to the queue
+        if (elemNum == capacity) {
+           resize();
         }
-        if(tail == capacity - 1){
-            a[0] = elem;
+        if (tail == capacity ) {
             tail = 0;
-            elem_num++;
+            q[tail++] = elem;
+            elemNum++;
         }
-        else{
-            a[tail+1] = elem;
+        else {
+            q[tail] = elem;
             tail++;
-            elem_num++;
+            elemNum++;
         }  
     }
     
-    public int dequeue() {
+    public int dequeue() { //delete an element from the queue
         if (isEmpty())
-             return 0;
-        head++;
-        elem_num--;
-        return a[head];
+            return 0;
+        elemNum--;
+        return q[head++];
     }
     
-    private void resize(int newSize){
-        int [] newArray = new int[newSize];
-        if (tail >= head){
+    private void resize() { // double array size
+        int oldCapacity = capacity;
+        capacity *= 2;
+        int [] newArray = new int[capacity];
+        if (tail > head) {
             int j = 0;
-            for (int i = head; i <= tail; i++){
-               newArray[j] = a[i];
+            for (int i = head; i <= tail - 1; i++) {
+               newArray[j] = q[i];
                j++;   
             }
         }
-        else{
-            int j=0;
-            for (int i = head; i < capacity;i++){
-                newArray[j] = a[i];
+        else {
+            int j = 0;
+            for (int i = head; i < oldCapacity; i++) {
+                newArray[j] = q[i];
                 j++;
             }
-            for (int i = 0; i <= tail; i++){
-                newArray[j] = a[i];
+            for (int i = 0; i < tail; i++) {
+                newArray[j] = q[i];
                 j++;
             }
             
         }
-        a = newArray;
+        q = newArray;
         head = 0;
-        tail = elem_num - 1;
+        tail = elemNum;
     }
     
-    public int size(){
-        return elem_num;
+    public int size() { // size of the queue
+        return elemNum;
     }
+    
+    public String toString() {
+        String st = "";
+        for (int i = 0; i < capacity; i++) {
+           st = st + new Integer(q[i]).toString() + " -> ";
+        }
+        return st;    
+    }    
 }
